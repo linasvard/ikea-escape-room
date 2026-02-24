2d9c7a727515e44c70a39bcd7ab879c07f877af4
linasvard <linajsvard@hotmail.com>
2026-02-24 12:09:20 +0100

    ✨🚧 Form works

--- src/rooms/room3.ts
+++ src/rooms/room3.ts
@@ -11,3 +11,36 @@ export default function initRoom3() {
 
   // övrig kod för rum 3
 }
+
+const regexHylla = /^15$/;
+const regexFack = /^42$/;
+
+const formFackHylla = document.querySelector("#guessHyllaFackSection") as HTMLFormElement;
+const fackInput = document.querySelector("#fackInput") as HTMLInputElement;
+const hyllaInput = document.querySelector("#hyllaInput") as HTMLInputElement;
+
+function validateHyllaInput(value: string): boolean {
+  return regexHylla.test(value);
+}
+
+function validateFackInput(value: string): boolean {
+  return regexFack.test(value);
+}
+
+function checkAndSubmitHyllaFack() {
+  const hyllaValid = validateHyllaInput(hyllaInput.value);
+  const fackValid = validateFackInput(fackInput.value);
+
+  // visa grön eller röd - för testning
+  hyllaInput.style.background = hyllaValid ? "green" : "black";
+  fackInput.style.background = fackValid ? "green" : "black";
+
+  if (hyllaValid && fackValid) {
+    formFackHylla.submit();
+  }
+}
+
+// Lyssnar på funktionen
+hyllaInput.addEventListener("input", checkAndSubmitHyllaFack);
+fackInput.addEventListener("input", checkAndSubmitHyllaFack);
+
