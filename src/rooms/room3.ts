12b2444e0c6e6dfd288103946161c6a7a197a1b5
linasvard <linajsvard@hotmail.com>
2026-02-26 15:01:42 +0100

    🚧 Rendering av options

--- src/rooms/room3.ts
+++ src/rooms/room3.ts
@@ -15,7 +15,7 @@ export default function initRoom3() {
 const regexHylla = /^15$/;
 const regexFack = /^42$/;
 
-const formFackHylla = document.querySelector("#guessHyllaFackSection") as HTMLFormElement;
+// const formFackHylla = document.querySelector("#guessHyllaFackSection") as HTMLFormElement;
 const fackInput = document.querySelector("#fackInput") as HTMLInputElement;
 const hyllaInput = document.querySelector("#hyllaInput") as HTMLInputElement;
 
@@ -40,14 +40,53 @@ function checkAndSubmitHyllaFack() {
     document.querySelector("#room3Part2")?.classList.remove("hidden");
   }
 }
-
-// Del två
-
-
-
 // Lyssnar på funktionen
 hyllaInput.addEventListener("input", checkAndSubmitHyllaFack);
 fackInput.addEventListener("input", checkAndSubmitHyllaFack);
 
 
+// Del två
 
+interface IOptionsRoom3 {
+  id: number;
+  name: string;
+  type: string;
+  img: string;
+}
+
+function renderRoom3Options(items: IOptionsRoom3[]): void {
+  const optionsContainerRoom3 = document.querySelector("#guessFurniture") as HTMLDivElement;
+  if (!optionsContainerRoom3) return;
+
+  optionsContainerRoom3.innerHTML = ""; // Rensa tidigare innehåll
+
+  optionsContainerRoom3.innerHTML = items.map((item) => `
+    <div class="optionContainer"><img src="${item.img}" alt="${item.name}" data-id="${item.id}" class="furniture-option">
+    <p>${item.id}</p></div>
+  `)
+  .join("");
+}
+
+renderRoom3Options(optionsRoom3);
+
+// 
+
+/* function renderFurnitureOptions(options: IOptionsRoom3[]) {
+  const furnitureOptionsContainer = document.querySelector("#guessFurniture") as HTMLDivElement;
+  furnitureOptionsContainer.innerHTML = ""; // Rensa tidigare innehåll
+  options.forEach(option => {
+    const imgElement = document.createElement("img");
+    imgElement.src = option.img;
+    imgElement.alt = option.name;
+    imgElement.dataset.id = option.id.toString();
+    imgElement.addEventListener("click", () => {
+      // Här kan du lägga till logik för vad som händer när en möbel klickas på
+      console.log(`Vald möbel: ${option.name}`);
+    });
+    furnitureOptionsContainer.appendChild(imgElement);
+  }
+  );
+}
+
+// Anropa funktionen för att rendera möbelalternativen
+renderFurnitureOptions(optionsRoom3);  */
