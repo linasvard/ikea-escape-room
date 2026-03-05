fd8ba8325c43320cc215d4618dc90c149939a199
Erik <egrahnmusic@gmail.com>
2026-03-05 11:42:47 +0100

    🔧 Update 1.1 to vite config base, to try to fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@
  * VITE CONFIG — ÄNDRA INTE UTAN ATT PRATA MED GRUPPEN
  * ============================================
  * Inställningar för Vite (byggverktyget).
- * base: "/fed25d-fed25d-js-intro-grupparbete-fed25d-js-intro-grupparbete-mall/" gör att sökvägar fungerar på GitHub Pages för detta repo.
+ * base: "/fed25d-js-intro-grupparbete-the-vorticon-vikings/" gör att sökvägar fungerar på GitHub Pages för detta repo.
  */
 
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Medieinstitutet/fed25d-js-intro-grupparbete-the-vorticon-vikings/",
+  base: "/fed25d-js-intro-grupparbete-the-vorticon-vikings/",
   build: {
     outDir: "dist",
   },
